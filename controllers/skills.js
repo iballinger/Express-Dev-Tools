b98fa0f92708ac81854b362726346b4a9c69b1cb
iballinger <ilerdonballinger@gmail.com>
2022-04-14 15:50:35 -0700

    First attempt at part 2

--- controllers/skills.js
+++ controllers/skills.js
@@ -14,3 +14,14 @@ function index(req, res) {
     const skills = Skills.getAll();
     res.render('skills/index', { skills });
 }
+
+function create(req, res) {
+    Skills.create(req.body);
+    res.redirect('/skills');
+}
+
+function deleteSkill(req, res) {
+    Skill.deleteOne(req.params.id);
+    // When data has been changed, we redirect (POST, PUT, DELETE requests)
+    res.redirect('/skills');  // Provide the PATH, not a template when redirecting
+}
--- models/skills.js
+++ models/skills.js
@@ -1,8 +1,8 @@
 const skills = [
-    {id: 100000, skill: 'CSS', known: true},
-    {id: 100001, skill: 'HTML', known: true},
-    {id: 100002, skill: 'Javascript', known: true},
-    {id: 100003, skill: 'Python', known: false},
+    { id: 100000, skill: 'CSS', known: true },
+    { id: 100001, skill: 'HTML', known: true },
+    { id: 100002, skill: 'Javascript', known: true },
+    { id: 100003, skill: 'Python', known: false },
 ]; //Remember that id is currently arbitrary, there's no hash function.
 
 module.exports = {
@@ -17,4 +17,16 @@ function getOne(id) {
 
 function getAll() {
     return skills;
+}
+
+function deleteOne(id) {
+    id = parseInt(id);
+    const idx = skills.findIndex((skill) => skill.id === id);
+    skills.splice(idx, 1);
+}
+
+function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skill.done = false;
+    skills.push(skill);
 }
